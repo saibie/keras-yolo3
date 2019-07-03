290383fce49b73ef4e83c2314d402e2f722dd134
saibie <saibie1677@gmail.com>
2019-07-03 16:52:02 +0900

    SeoKeras

--- train.py
+++ train.py
@@ -14,9 +14,9 @@ from yolo3.utils import get_random_data
 
 
 def _main():
-    annotation_path = 'train.txt'
+    annotation_path = 'labelbox.txt'#'train.txt'
     log_dir = 'logs/000/'
-    classes_path = 'model_data/voc_classes.txt'
+    classes_path = 'model_data/highway_classes.txt'
     anchors_path = 'model_data/yolo_anchors.txt'
     class_names = get_classes(classes_path)
     num_classes = len(class_names)
